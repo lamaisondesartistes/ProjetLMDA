2116b7dc9977aa409b5aeeabbc475d7a312c8e24
denysl152 <chabinlouves199@gmail.com>
2018-01-19 15:14:29 -0400

    add LegalNotices

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route } from 'react-router-dom';
+import {Route,Switch,BrowserRouter} from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Accueil  from './Components/Accueil';
@@ -8,19 +8,27 @@ import Presentation from './Components/Presentation';
 import Produits from './Components/Produits';
 import Contact from './Components/Contact';
 import NotFound from './Components/Notfound';
+import MentionsLégales from './Components/Mentionslegal';
+import Mentionslegal from './Components/Mentionslegal';
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
         <div>
-          <Header/>
+       <Header/>
+       <Switch>
           <Route exact path="/" component={Accueil} />
-          <Route path="/Createurs" component={Createurs} />
-          <Route path="/Presentation" component={Presentation}/>
-          <Route path="/Produits" component={Produits} />
-          <Route path="/Contact" component={Contact} />
-          <Route path="*" component={NotFound} />
+          <Route exact path="/Createurs" component={Createurs} />
+          <Route exact path="/Presentation" component={Presentation}/>
+          <Route exact path="/Produits" component={Produits} />
+          <Route exact path="/Contact" component={Contact} />
+          <Route path="/Mentionslegales" component={Mentionslegal}/>
+          <Route component={NotFound} />
+          
+          </Switch>
         </div>
+        </BrowserRouter>
     );
   }
 }
---
+++ src/Components/Mentionslegal.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './Mentionslegal.css';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="Legal-Notice">
+      <div className="Container-Info">
+      <h1 className="Title-lign">Mentions Légales</h1>
+    <div className="Block-Info">
+    <h3 className="Container-LégalNotice">La Maison Des Artistes</h3>     
+   <p className="Info-Business">Le site lamaisondesartistes.com est édité par :</p>
+   <p className="Info-Business"><b>Simplon Marie-Galante</b></p>
+   <p className="Info-Business">Section Tivoli 97112 Grand-Bourg</p>
+   <p className="Info-Business">La Maison des Artistes est une Entreprise qui conçoit des objets artisanaux.</p>
+   <p className="Info-Business"><b>Adresse de l'établissement :</b> Rue de la Savane 97112 Grand-Bourg</p>
+   <p className="Info-Business"><b>Téléphone:</b> 000000000.</p>
+   <p className="Info-Business"><b>Email:</b> lamaisondesartistes@gmail.com</p>
+   <p className="Info-Business"><b>Numéro d'immaculation pour les activités artisales :</b> </p>
+   <p className="Info-Business"><b>Le Directeur de cette publication est </b></p><p>Simplon MG. </p >
+     
+   <hr size="1" align="left" width="60%" color="black" ></hr>
+  
+  <h3 className="Container-LégalNotice">Hébergement</h3>
+  
+    <p className="Info-Business">Le site lamaisondesartistes.com est hébergé par : </p>
+   <p className="Info-Business"><b></b></p>
+   <p className="Info-Business"><b>Adresse :</b></p>
+   <p className="Info-Business"><b>Téléphone :</b></p>
+   </div>
+   </div>
+   </div>
+    );
+  }
+}
+
+export default App;
