fb802e2ccc112ce2a03c175b290a2de77362dcb1
Evinym <Evinym@outlook.fr>
2018-02-06 14:13:51 -0400

    modified the slider

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import Slidero from 'react-slick';
 import './Slider.css';
-
+import Slide1 from '../Images/ISlid1.png';
 import Slide2 from '../Images/ISlid2.png';
 import Slide3 from '../Images/ISlid3.png';
 import Slide4 from '../Images/ISlid4.png';
 import Slide5 from '../Images/ISlid5.png';
 import Slide6 from '../Images/ISlid6.png';
-import Slide7 from '../Images/ISlid1.png';
 import Slide8 from '../Images/ISlid8.png';
-import Slide9 from '../Images/ISlid9.png';
-import Slide1 from '../Images/ISlid1.png';
-
+import Slide7 from '../Images/ISlid7.png';
 
 class Rick extends Component {
     render() {
@@ -19,26 +16,47 @@ class Rick extends Component {
             dots: true,
             infinite: true,
             speed: 500,
-            slidesToShow: 6,
+            slidesToShow: 5,
             slidesToScroll: 1,
             autoplay : true,
             dots: true,
             autoplay: true,
             autoplaySpeed: 5000,
+            responsive: [   {
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    infinite: true,
+                    dots: true
+                  } }, {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                  } }, {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                } ]
           };
         return (
-        <Slidero {...settings} className="sliderCoponent" >
+        <div>
+        <Slidero {...settings} className="sliderCoponent ImgModif" >
                         
-            <div className="Leslo"><img className="sliderCoponentImg " src={Slide2} alt="partenaire slide" width="20px" /></div>
-            <div className="Leslo"><img className="sliderCoponentImg " src={Slide4} alt="partenaire slide" width="20px" /></div>           
-            <div className="Leslo"><img className="sliderCoponentImg " src={Slide3} alt="partenaire slide" width="20px" /></div>
-            <div className="Leslo"><img className="sliderCoponentImg " src={Slide9} alt="partenaire slide" width="20px" /></div>
-            <div className="Leslo"><img className="sliderCoponentImg " src={Slide1} alt="partenaire slide" width="20px" /></div>
-            <div className="Leslo"><img className="sliderCoponentImg " src={Slide8} alt="partenaire slide" width="20px" /></div>
-            <div className="Leslo"><img className="sliderCoponentImg " src={Slide5} alt="partenaire slide" width="20px" /></div>
-            <div className="Leslo"><img className="sliderCoponentImg " src={Slide6} alt="partenaire slide" width="20px" /></div>
+            <div><img className="sliderCoponentImg" src={Slide2} alt="partenaire slide" /></div>
+            <div><img className="sliderCoponentImg" src={Slide4} alt="partenaire slide" /></div>           
+            <div><img className="sliderCoponentImg" src={Slide3} alt="partenaire slide" /></div>
+            <div><img className="sliderCoponentImg" src={Slide7} alt="partenaire slide" /></div>
+            <div><img className="sliderCoponentImg" src={Slide1} alt="partenaire slide" /></div>            
+            <div><img className="sliderCoponentImg" src={Slide5} alt="partenaire slide" /></div>
+            <div><img className="sliderCoponentImg" src={Slide8} alt="partenaire slide" /></div>
+            <div><img className="sliderCoponentImg" src={Slide6} alt="partenaire slide" /></div>
             
           </Slidero>
+          </div>
         );
     }
 }
