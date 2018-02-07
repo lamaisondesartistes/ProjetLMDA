f7c9931baf2abe75f2761e5b2c1c39cc9b8f2032
Kenpachi971 <mrcapharsie.m@gmail.com>
2018-02-07 15:12:14 -0400

    workshop Home

--- src/Components/CardProduit.js
+++ src/Components/CardProduit.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
-
- 
 class CardMini extends Component {
   state={
     visibility:'hidden',
-    heightImage:'240px',
-    witdhImage:'320px'
   }
   showDesc=()=>{
-
     this.setState({
       visibility : 'visible',
-      heightImage:'270px',
-      witdhImage:'350px',
+      
     })
   }
   hideDesc=()=>{
     this.setState({
       visibility:'hidden',
-      heightImage:'240px',
-      witdhImage:'320px'
     })
   }
   
@@ -28,20 +21,18 @@ class CardMini extends Component {
     const Imageuh = {
       backgroundImage:`url(${this.props.src})`,
       backgroundSize:'cover',
-      height:'239px',
+      
     }
     return (
       <div className="Card"  >
-          <div className="div-Card-Image">
+          <NavLink to="/Produits"className="div-Card-Image">
               <div className="imgProduit" style={Imageuh} onMouseOver={this.showDesc} onMouseLeave={this.hideDesc}>
               <p className="TitreDeProduit" style={{visibility:this.state.visibility}} >{this.props.nomProduit}</p>
-              <p style={{visibility:this.state.visibility}} >{this.props.descriProduit}</p>
-              </div>              
-              
-          </div>
+              <p className="TryProduct" style={{visibility:this.state.visibility}} >{this.props.descriProduit}</p>
+              </div>
+          </NavLink>
       </div>
     );
   }
 }
-
 export default CardMini;
