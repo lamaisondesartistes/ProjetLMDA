a81d4fb9f67907fa8bb36c091be04ff7aef77ec9
yveschristophe <saban.yveschristophe@gmail.com>
2018-02-08 13:56:19 -0400

    Content on jumbotron

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -25,7 +25,9 @@ class App extends Component {
     </div>
     <img src={lambi} alt="conque à lambi" className="imgLMDA"/>
     <h1>QUI SOMMES-NOUS ?</h1>
-    <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des pa</p>
+    <p> Dans le cadre du concours « Entreprendre en Lycée », le lycée H. BASTARAUD présente :
+                    «  LA MAISON DES ARTISTES » création, production, vente et formation aux métiers de l’artisanat
+                    d’art.</p>
     </div>
     );
   }
