e419076a6f0d5a1d88ed6a0c590f0bf3de9b1042
RiinaYoyo <yoann.forthulman@gmail.com>
2018-02-15 15:20:08 -0400

    solve bugs

--- src/Header.js
+++ src/Header.js
@@ -17,7 +17,7 @@ class Header extends Component {
             <NavLink to="/Accueil"><img src={LMDA} alt="logo" className="LMDA"/></NavLink>
             </div>
             <div className="divLiens">
-                <NavLink to="/Accueil" className="Liens">ACCUEIL</NavLink>
+                <NavLink to="/" className="Liens">ACCUEIL</NavLink>
                 <NavLink to="/Presentation" className="Liens">PRESENTATION</NavLink>
                 <NavLink to="/Ateliers" className="Liens">ATELIERS</NavLink>
                 <NavLink to="/Createurs" className="Liens">CREATEURS</NavLink>
