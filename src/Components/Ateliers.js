83358aaa32ec869ae65576adcbd1a8a0e598b461
GauthierGinier <gauthier.ginier@gmail.com>
2018-02-19 16:04:42 -0400

    maj des images

--- src/Components/Ateliers.js
+++ src/Components/Ateliers.js
@@ -6,32 +6,32 @@ import './CardProduits2.css';
 
 const products = [
   {
-    url: './images/oiseaux.jpg',
+    url: './images/1.jpg',
     nom: 'Oiseaux',
     descri: 'Réalisé à partir de feuilles de cocotiers.',
     poste: 'objets déco',
   }, {
-    url: 'images/boule.jpg',
+    url: 'images/2.jpg',
     nom: 'Boule',
     descri: 'Réalisé',
     poste: 'objets déco',
   }, {
-    url: 'images/boule2.jpg',
+    url: 'images/3.jpg',
     nom: 'Boules',
     descri: 'Élève très impliqué et sérieux.',
     poste: 'objets déco',
   }, {
-    url: 'images/cerfs.jpg',
+    url: 'images/4.jpg',
     nom: 'Cerfs en bois',
     descri: 'Indispensable à la promotion, fait régner la joie et la bonne humeur.',
     poste: 'objets déco',
   }, {
-    url: 'images/collier.jpg',
+    url: 'images/5.jpg',
     nom: 'Collier de graines',
     descri: 'Réalisé à partir de graines du \'Péyi\'.',
     poste: 'bijoux',
   }, {
-    url: 'images/graines.jpg',
+    url: 'images/6.jpg',
     nom: 'Graines diverses',
     descri: 'Réalisé à partir de graines du \'Péyi\'.',
     poste: 'objets déco',
