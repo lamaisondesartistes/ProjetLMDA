ab4e3c2de1754226e002c2a890327d9a3b3dd6a8
Shan971 <stonixkxf@gmail;com>
2018-02-19 12:15:53 +0100

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,32 +9,25 @@ class Footer extends Component {
         return (
             <div className="Footer">
                 <footer className="contenu">
-                    <div className="home"><i className="fa fa-home" aria-hidden="true">lamaisondesartistesmg@gmail.com</i></div>
-                    <div className="call"><i className="fa fa-phone" aria-hidden="true"> 0590 97 90 42</i></div>
-                    <div className="alignement">
-                        <div className='icon social fb'><i className='fa fa-facebook'></i></div>
-                        <div className='icon social tw'><i className='fa fa-twitter'></i></div>
-                        <div className='icon social in'><i className='fa fa-instagram'></i></div>
-                    </div>
+                    <div className="mail-footer"><i className="fas fa-home"></i>lamaisondesartistesmg@gmail.com</div>
+                    <div className="tel-footer"><i className="fas fa-phone-volume"></i> 0590 97 90 42</div>
                 </footer>
-                <div className="container">
-                    <NavLink to="/" className="color">Accueil</NavLink>
-                    <NavLink to="/Presentation" className="color">Presentation</NavLink>
-                    <NavLink to="/Produits" className="color">Produits</NavLink>
-                    <NavLink to="/Createurs" className="color">Createurs</NavLink>
-                    <NavLink to="/Contact" className="color">Contact</NavLink>
+                <div className="social-footer">
+                    <i className="fab fa-facebook icon-f"></i>
+                    <i className="fab fa-twitter icon-f"></i>
+                    <i className="fab fa-instagram icon-f"></i>
                 </div>
-                <div className="container2">
+                <div className="foot-contain2">
                     <div>
                         <p>Made with <i className="fa fa-heart"></i> by Simplon MG </p>
                     </div>
                     <div>
-                        <NavLink to="/MentionsLegales" className="color">MentionsLégales</NavLink> <i className="fa fa-arrow-right" ></i>
+                        <p>MentionsLégales<i className="fa fa-arrow-right"></i></p>
                     </div>
 
                 </div>
 
-            </div> 
+            </div>
         )
     }
 }
