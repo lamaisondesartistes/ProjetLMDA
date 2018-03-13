70e38419eca4b209ecdf0a04e20a61bffc23c750
Kenpachi971 <mrcapharsie.m@gmail.com>
2018-03-13 15:45:42 -0400

    Picture Compressed

--- src/Components/DataProduit.js
+++ src/Components/DataProduit.js
@@ -1,35 +1,35 @@
-import IMG3 from "../Images/LMDA3.jpg"
-import IMG4 from "../Images/LMDA4.jpg"
-import IMG5 from "../Images/LMDA5.jpg"
-import nature from "../Images/pagelines.png"
-import graduation from "../Images/graduation-cap1.png"
-import leanpub from "../Images/leanpub1.png"
+import IMG3 from '../Images/LMDA3.png';
+import IMG4 from '../Images/LMDA4.png';
+import IMG5 from '../Images/LMDA5.png';
+import nature from '../Images/pagelines.png';
+import graduation from '../Images/graduation-cap1.png';
+import leanpub from '../Images/leanpub1.png';
 
+var Produits = [
+	{
+		src: IMG3,
+		alt: 'Armoire a Palettes',
+		nom: 'NATURE :',
+		icone: nature,
+		descr:
+			"La nature joue un rôle vital dans le monde car elle nous fourni l'air requis afin de nous maintenir en vie. Elle nous protège et nous apporte un environnement sain, de l'eau, des matières premières. Nous utilisons cette même nature afin de construires divers choses...",
+	},
+	{
+		src: IMG4,
+		alt: 'Armoire a Palettes',
+		nom: 'LYCEE :',
+		icone: graduation,
+		descr:
+			"Le Lycée Hyacinthe Bastaraud est un établissement polyvalent public, mixte et laïque et contient un internat. Ce lycée propose divers activités tel que le Brevet d'Initiation Aéronautique, l'Union National du Sport Scolaire, la préparation du Test Of English for International Communication...",
+	},
+	{
+		src: IMG5,
+		alt: 'Armoire a Palettes',
+		nom: 'ENTREPRENDRE EN LYCEE :',
+		icone: leanpub,
+		descr:
+			"L’opération « Entreprendre En Lycée » permet de combler le fossé culturel existant entre l’école et l’entreprise, en démystifiant l’acte d’entreprendre. Grâce à cela, elle favorise la curiosité des jeunes à fonder des entreprises dans le futur, elle donne la possibilité aux collègues de collaborer entre eux et elle souscrit la valorisation de l'établissement",
+	},
+];
 
-var Produits=[
-    {
-        src:IMG3,
-        alt:"Armoire a Palettes",
-        nom:"NATURE :",
-        icone:nature,
-        descr:"La nature joue un rôle vital dans le monde car elle nous fourni l'air requis afin de nous maintenir en vie. Elle nous protège et nous apporte un environnement sain, de l'eau, des matières premières. Nous utilisons cette même nature afin de construires divers choses..."
-    },
-    {
-        src:IMG4,
-        alt:"Armoire a Palettes",
-        nom:"LYCEE :",
-        icone:graduation,
-        descr:"Le Lycée Hyacinthe Bastaraud est un établissement polyvalent public, mixte et laïque et contient un internat. Ce lycée propose divers activités tel que le Brevet d'Initiation Aéronautique, l'Union National du Sport Scolaire, la préparation du Test Of English for International Communication..."
-    },
-    {
-        src:IMG5,
-        alt:"Armoire a Palettes",
-        nom:"ENTREPRENDRE EN LYCEE :",
-        icone:leanpub,
-        descr:"L’opération « Entreprendre En Lycée » permet de combler le fossé culturel existant entre l’école et l’entreprise, en démystifiant l’acte d’entreprendre. Grâce à cela, elle favorise la curiosité des jeunes à fonder des entreprises dans le futur, elle donne la possibilité aux collègues de collaborer entre eux et elle souscrit la valorisation de l'établissement"
-    }
-    
-]
-
-
-export default Produits
+export default Produits;
