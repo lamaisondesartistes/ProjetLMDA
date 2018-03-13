aa91ed51b23b055e9f7de772fc48703a245f1797
Evinym <Evinym@outlook.fr>
2018-03-13 13:43:10 -0400

    correction of disposition

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -54,7 +54,7 @@ class theSlider extends Component {
 
         return (
         <div>
-          <h1 className="sliderCo">Partenaires</h1>
+          <h2 className="sliderCo">Partenaires</h2>
         <Slidero {...settings} className="sliderCoponent" >
           {data.map((el, i)=>{
             return(
