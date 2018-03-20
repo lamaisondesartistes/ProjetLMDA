b2a99fbc9f14071eb7ff29f146aa06a8cb3a97e1
Boris <boris.pozzo@gmail.com>
2018-03-20 16:45:55 -0400

    fix link images

--- src/Components/DataProduit2.js
+++ src/Components/DataProduit2.js
@@ -1,45 +1,45 @@
 import IMG1 from "../Images/LMDA1.jpg"
 import IMG2 from "../Images/LMDA2.jpg"
-import IMG3 from "../Images/LMDA3.jpg"
-import IMG4 from "../Images/LMDA4.jpg"
-import IMG5 from "../Images/LMDA5.jpg"
+import IMG3 from "../Images/LMDA3.png"
+import IMG4 from "../Images/LMDA4.png"
+import IMG5 from "../Images/LMDA5.png"
 
 
 
 
-var Produits=[
+var Produits = [
     {
-        src:IMG1,
-        alt:"Table Post-Cuite",
-        nom:"ACCESSOIRES ET OBJETS DECORATIFS :",
-        descr:"Atelier de création, production d'accesoires et objets décoratifs à base de calebasses, bois flotté, graines, sables et autres éléments naturels. A savoir : kwi, lampes, présentoir boucle d'oreille...",
+        src: IMG1,
+        alt: "Table Post-Cuite",
+        nom: "ACCESSOIRES ET OBJETS DECORATIFS :",
+        descr: "Atelier de création, production d'accesoires et objets décoratifs à base de calebasses, bois flotté, graines, sables et autres éléments naturels. A savoir : kwi, lampes, présentoir boucle d'oreille...",
     }
-    ,{
-        src:IMG2,
-        alt:"fauteuil a Roue",
-        nom:"BIJOUX ET ACCESSOIRES DE MODE :",
-        descr:"Atelier de création, production de bijoux et accessoires de mode à base de graines naturelles (oeil de boeuf et de bourrique, savonette, graine de job, canique...), de coquillages et coraux et autres éléments naturels (dents de cochon, bambou...)",
+    , {
+        src: IMG2,
+        alt: "fauteuil a Roue",
+        nom: "BIJOUX ET ACCESSOIRES DE MODE :",
+        descr: "Atelier de création, production de bijoux et accessoires de mode à base de graines naturelles (oeil de boeuf et de bourrique, savonette, graine de job, canique...), de coquillages et coraux et autres éléments naturels (dents de cochon, bambou...)",
     }
     ,
     {
-        src:IMG3,
-        alt:"Armoire a Palettes",
-        nom:"ATELIER POUR ENFANTS :",
-        descr:"LA MAISON DES ARTISTES se veut être aussi, un pôle d’animation en réalisant des ateliers pédagogiques à l’attention des écoles maternelles et primaires qui permettront une sensibilisation des tout petits, à notre environnement de part la richesse qu’il nous procure."
+        src: IMG3,
+        alt: "Armoire a Palettes",
+        nom: "ATELIER POUR ENFANTS :",
+        descr: "LA MAISON DES ARTISTES se veut être aussi, un pôle d’animation en réalisant des ateliers pédagogiques à l’attention des écoles maternelles et primaires qui permettront une sensibilisation des tout petits, à notre environnement de part la richesse qu’il nous procure."
     },
     {
-        src:IMG4,
-        alt:"Armoire a Palettes",
-        nom:"GAULETTE :",
-        descr:"Atelier de création, production d’objets d’ameublement et de décoration à base de gaulette* (*arbre merisier, bois d’inde, bois chandelle) : tables basses, des lampes, des objets décorations ..."
+        src: IMG4,
+        alt: "Armoire a Palettes",
+        nom: "GAULETTE :",
+        descr: "Atelier de création, production d’objets d’ameublement et de décoration à base de gaulette* (*arbre merisier, bois d’inde, bois chandelle) : tables basses, des lampes, des objets décorations ..."
     },
     {
-        src:IMG5,
-        alt:"Armoire a Palettes",
-        nom:"ACCESSOIRISATION DE VENTEMENT :",
-        descr:"Atelier d’accessoirisation. Les boubous créoles sont réalisés par notre partenaire, et accessoirisés avec des graines naturelles, des coquillages, ou autres éléments naturels."
+        src: IMG5,
+        alt: "Armoire a Palettes",
+        nom: "ACCESSOIRISATION DE VENTEMENT :",
+        descr: "Atelier d’accessoirisation. Les boubous créoles sont réalisés par notre partenaire, et accessoirisés avec des graines naturelles, des coquillages, ou autres éléments naturels."
     }
-    
+
 ]
 
 
