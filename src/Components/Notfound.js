b4263906746727cbcdcd154eb4aa5ece2e82269c
Evinym <Evinym@outlook.fr>
2018-03-20 16:43:49 -0400

    finished 404 error

--- src/Components/Notfound.js
+++ src/Components/Notfound.js
@@ -8,10 +8,10 @@ class NOTFOUND extends Component {
     return (
       <div className="page-error-container">
         <div className="page-error-container2" >
-          <div className="dinheader">
-          <img className="dinheaders" src={LMDA} alt="logo" height="178px" width="414px"/>
+          <div className="logoLMDA1">
+          <img className="logoLMDA2" src={LMDA} alt="logo" height="178px" width="414px"/>
           </div>
-          <div className="dinheador dinheaderset">
+          <div className="logoLMDA3 logoLMDA4">
           <h1 className="oups" >Oupss!</h1>
           <p>La page que vous recherchez n’existe pas ou plus :</p>          
           <p>Utiliser le menu ci-dessus ou retourner à la page d’accueil pour tenter de trouver l’article que vous chercher.</p>
