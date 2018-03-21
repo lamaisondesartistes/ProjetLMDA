b9c687af5179ae1f369fd51073ae58f906c7f853
Kenpachi971 <mrcapharsie.m@gmail.com>
2018-03-20 21:30:20 -0400

    Header change

--- src/Header.js
+++ src/Header.js
@@ -2,34 +2,44 @@ import React, { Component } from 'react';
 import LMDA from './Images/LaMaisonDesArtistes.png';
 import { NavLink } from 'react-router-dom';
 
-
 class Header extends Component {
-    render() {
-        return (
-            <div className="Header">
-
-           <div className="Reseaux">
-            <div className="iconReseaux"><i className="fab fa-facebook" /></div>
-            <div className="iconReseaux"><i className="fab fa-instagram" /></div>
-            <div className="iconReseaux"><i className="fab fa-twitter" /></div>
-          </div>
-            <div className="divLMDA">
-            <img src={LMDA} alt="logo" className="LMDA"/>
-            </div>
-            <div className="divLiens">
-                <NavLink to="/" className="Liens">ACCUEIL</NavLink>
-                <NavLink to="/Presentation" className="Liens">PRESENTATION</NavLink>
-                <NavLink to="/Ateliers" className="Liens">ATELIERS/PRODUITS</NavLink>
-                <NavLink to="/Createurs" className="Liens">CREATEURS</NavLink>
-                <NavLink to="/Contact" className="Liens">CONTACT</NavLink>
-              
-                
-          </div>
-          </div>
-    
-        
-        )
-    }
-};
+	render() {
+		return (
+			<div className="Header">
+				<div className="Reseaux">
+					<div className="iconReseaux">
+						<i className="fab fa-facebook" />
+					</div>
+					<div className="iconReseaux">
+						<i className="fab fa-instagram" />
+					</div>
+					<div className="iconReseaux">
+						<i className="fab fa-twitter" />
+					</div>
+				</div>
+				<div className="divLMDA">
+					<img src={LMDA} alt="logo" className="LMDA" />
+				</div>
+				<div className="divLiens">
+					<NavLink to="/Accueil" className="Liens">
+						ACCUEIL
+					</NavLink>
+					<NavLink to="/Presentation" className="Liens">
+						PRESENTATION
+					</NavLink>
+					<NavLink to="/Ateliers" className="Liens">
+						ATELIERS/PRODUITS
+					</NavLink>
+					<NavLink to="/Createurs" className="Liens">
+						CREATEURS
+					</NavLink>
+					<NavLink to="/Contact" className="Liens">
+						CONTACT
+					</NavLink>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Header;
