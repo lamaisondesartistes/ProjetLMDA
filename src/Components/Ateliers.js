b828b74ce03596a70d4d81dcf34868fae21052bc
Megane <gr.benedith@gmail.com>
2018-04-10 10:48:53 -0400

    last modification

--- src/Components/Ateliers.js
+++ src/Components/Ateliers.js
@@ -1,276 +1,92 @@
-import React, { Component } from "react";
-import Footer from "./Footer";
-import TestImage from "../Images/contact3.jpg";
-import Cards from "./Cards2";
-import "./CardProduits2.css";
-import "./Ateliers.css";
-//import
-import IMG1 from "../Images/LMDA1.jpg";
-import IMG2 from "../Images/LMDA2.jpg";
-import IMG3 from "../Images/LMDA3.png";
-import IMG4 from "../Images/LMDA4.png";
-import IMG5 from "../Images/LMDA5.png";
-
-//
-
-const listPictureAteliers = [
-  {
-    images: IMG1,
-    title: "GAULETTE",
-    describe:
-      "Atelier de création, production d’objets d’ameublement et de décoration à base de gaulette* (*arbre merisier, bois d’inde, bois chandelle) : tables basses, des lampes, des objets décorations ..."
-  },
-  {
-    images: IMG2,
-    title: "ACCESSOIRISATION DE VETEMENT",
-    describe:
-      "Atelier d’accessoirisation. Les boubous créoles sont réalisés par notre partenaire, et accessoirisés avec des graines naturelles, des coquillages, ou autres éléments naturels."
-  },
-  {
-    images: IMG3,
-    title: "BIJOUX ET ACCESSOIRES DE MODE",
-    describe:
-      "Atelier de création, production de bijoux et accessoires de mode à base de graines naturelles (œil de bœuf et de bourrique, savonette, graine de job, canique...), de coquillages et coraux et autres éléments naturels (dents de cochon, bambou...)..."
-  },
-  {
-    images: IMG4,
-    title: "ACCESSOIRES ET OBJETS DECORATIFS",
-    describe:
-      "Atelier de création, production d’accessoires et objets décoratifs à base de calebasses, bois flotté, graines, sables et autres éléments naturels. A savoir : kwi, lampes, présentoir boucle d’oreille..."
-  },
-  {
-    images: IMG5,
-    title: "ATELIERS POUR ENFANTS",
-    describe:
-      "LA MAISON DES ARTISTES se veut être aussi, un pôle d’animation en réalisant des ateliers pédagogiques à l’attention des écoles maternelles et primaires qui permettront une sensibilisation des tout petits, à notre environnement de part la richesse qu’il nous procure."
-  }
-];
+import React, { Component } from 'react';
+import Footer from './Footer';
+import Cards from './Cards2';
+import './CardProduits2.css';
 
 const products = [
   {
-    url: "./images/1.jpg",
-    nom: "Collier Coquillage"
-  },
-  {
-    url: "images/2.jpg",
-    nom: "Collier Dent"
-  },
-  {
-    url: "images/3.jpg",
-    nom: "Oeil de boeuf"
-  },
-  {
-    url: "images/4.jpg",
-    nom: "Boucles d'oreilles"
-  },
-  {
-    url: "images/5.jpg",
-    nom: "Colliers divers"
-  },
-  {
-    url: "images/7.jpg",
-    nom: "Colliers de bambou"
-  },
-  {
-    url: "images/8.jpg",
-    nom: "Boucles d'oreille avec canique"
-  },
-  {
-    url: "images/6.jpg",
-    nom: "Boucles d'oreilles divers"
-  }
-];
+    url: './images/1.jpg',
+    nom: 'Collier Coquillage',
+  }, {
+    url: 'images/2.jpg',
+    nom: 'Collier Dent',
+  }, {
+    url: 'images/3.jpg',
+    nom: 'Oeil de boeuf',
+  }, {
+    url: 'images/4.jpg',
+    nom: 'Boucles d\'oreilles',
+  }, {
+    url: 'images/5.jpg',
+    nom: 'Colliers divers',
+  }, {
+    url: 'images/6.jpg',
+    nom: 'Boucles d\'oreilles divers',
+  }];
 class App extends Component {
   state = {
     modalVisibility: false,
     position: 0,
     nbr: 0
-  };
-  // function Next and previous
+  }
 
-  previousSlide = () => {
-    const { position } = this.state;
-    this.setState({
-      position: position - 1
-    });
-    if (position <= 0)
-      this.setState({
-        position: listPictureAteliers.length - 1
-      });
-  };
-
-  nextSlide = () => {
-    this.setState({
-      position: this.state.position + 1
-    });
-    if (this.state.position > listPictureAteliers.length - 2) {
-      this.setState({
-        position: 0
-      });
-    }
-  };
-
-  showModal = i => {
+  showModal = (i) => {
     this.setState({
       modalVisibility: true,
-      nbr: i
-    });
-  };
+      nbr: i,
+    })
+
+  }
   hideModal = () => {
     this.setState({
       modalVisibility: false
-    });
-  };
+    })
+  }
 
   render() {
+
+
     const { modalVisibility } = this.state;
-    const newMargin = this.state.position * -275;
-    const newMargins = this.state.position * -470;
     return (
+
+
       <div id="App">
-        <strong>
-          <p className="TextLoremH">
-            Marie-galante est riche de productions locales et de traditions
-            ancrées dans le patrimoine. Par conséquent LA MAISON DES ARTISTES se
-            fera un plaisir de contribuer à leur valorisation ainsi qu’à leur
-            promotion, à travers toutes nos créations.
-          </p>
-        </strong>
-        <div className="DivOfTitle2">
-          <p className="TextOfDivOfTitle2">ATELIERS</p>
-        </div>
+      <strong><p className="TextLoremH">Marie-galante est riche de productions locales et de
+        traditions ancrées dans le patrimoine. Par conséquent LA MAISON DES ARTISTES se fera un
+        plaisir de contribuer à leur valorisation ainsi qu’à leur promotion, à travers toutes nos
+        créations.
+      </p></strong>
+      <div className="DivOfTitle2">
+            <p className="TextOfDivOfTitle2">ATELIERS</p>
+      </div>
+      <Cards/>
+        {
+          modalVisibility ?
 
-        {/*PARTIE SLIDER ATELIER*/}
-
-        <div className="ateslider">
-          {/*PARTIE TITLE ATELIER*/}
-
-          <div className="boxtitle">
-            <ul style={{ marginLeft: newMargin }} className="list">
-              {listPictureAteliers.map((slide, i) => {
-                return (
-                  <li key={i}>
-                    <p className="atesentences">{slide.title}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-
-          {/*PARTIE IMAGES ATELIER*/}
-
-          <div className="box">
-            <a onClick={this.previousSlide} type="button">
-              <i className="fas fa-chevron-left" />
-            </a>
-            <div className="slider-container">
-              <ul style={{ marginLeft: newMargin }} className="list">
-                {listPictureAteliers.map((el, i) => {
-                  return (
-                    <li key={i}>
-                      <img
-                        className="atepicture"
-                        src={el.images}
-                        alt={el.title}
-                      />
-                    </li>
-                  );
-                })}
-              </ul>
+            <div id="modal-container" onClick={this.hideModal}>
+              <div id="creatorprofile" onClick={e => {
+                e.stopPropagation();
+              }  // Do something
+              }>
+                <img className="creatorpp" src={products[this.state.nbr].url} alt={products[this.state.nbr].nom} />
+                
+              </div>
+              <img alt="" id="croix" onClick={this.hideModal} src="https://smartbees.fr/assets/img/x-min.png" />
             </div>
-            <a onClick={this.nextSlide} type="button">
-              <i className="fas fa-chevron-right" onClick={this.nextSlide} />
-            </a>
-          </div>
 
-          {/*
-  LA PARTIE  DESCRIPTION ATELIER
-
-*/}
-
-          <div className="boxsentence">
-            <ul style={{ marginLeft: newMargins }} className="list">
-              {listPictureAteliers.map((slide, i) => {
-                return (
-                  <li key={i}>
-                    <p className="atesentence">{slide.describe}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </div>
-
-        {/*
-
-Partie a  ne pas toucher
-
-*/}
-
-        {modalVisibility ? (
-          <div id="modal-container" onClick={this.hideModal}>
-            <div
-              id="creatorprofile"
-              onClick={
-                e => {
-                  e.stopPropagation();
-                } // Do something
-              }
-            >
-              <img
-                className="creatorpp"
-                src={products[this.state.nbr].url}
-                alt={products[this.state.nbr].nom}
-              />
-            </div>
-            <img
-              alt=""
-              id="croix"
-              onClick={this.hideModal}
-              src="https://smartbees.fr/assets/img/x-min.png"
-            />
-          </div>
-        ) : null}
-
-        {/* liste des produits */}
-        {modalVisibility ? (
-          <div id="modal-container" onClick={this.hideModal}>
-            <div
-              id="creatorprofile"
-              onClick={
-                e => {
-                  e.stopPropagation();
-                } // Do something
-              }
-            >
-              <img
-                className="creatorpp"
-                src={products[this.state.nbr].url}
-                alt={products[this.state.nbr].nom}
-              />
-            </div>
-            <img
-              alt=""
-              id="croix"
-              onClick={this.hideModal}
-              src="https://smartbees.fr/assets/img/x-min.png"
-            />
-          </div>
-        ) : null}
-        <div className="DivOfTitle2">
+            : null
+        }
+        <div className="ImageNature2">
           <p className="TextOfDivOfTitle2">PRODUITS</p>
         </div>
         <ul id="Cards-container">
           {products.map((Card, i) => {
             return (
-              <li className="Cards" onClick={() => this.showModal(i)} key={i}>
-                <img className="creatorspp" src={Card.url} alt={Card.nom} />
-                <p>{Card.nom}</p>
-              </li>
-            );
+              <li className="Cards" onClick={() => this.showModal(i)} key={i}><img className="creatorspp" src={Card.url} alt={Card.nom} /><p>{Card.nom}</p></li>
+            )
           })}
         </ul>
-        <Footer />
+        <Footer/>
       </div>
     );
   }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,50 +1,68 @@
 import React, { Component } from 'react';
 import Footer from './Footer';
+//Insertion Map leaflet
+import { Map, TileLayer, Marker } from 'react-leaflet';
 import './Contact.css';
+
+
+
 class Contact extends Component {
+    state ={
+        zoom:20,
+        latitude: 15.88364,
+        longitude: -61.31296,
+    }
+
     render() {
+      
         return (
             <div>
-            <div className="BoxContainer" >
-            <strong><p className="styleText" id="TextLorem">
-                            Afin de porter notre pierre à l’édifice, et pour répondre à un appel à « Entreprendre en lycée »
-concours organisé par le « GIPDAIFI » nous lycée Hyacinthe Bastaraud de Marie-Galante avons choisi
-de mettre en place : « La Maison des Artistes », entreprise à caractère commerciale qui a pour but de
-créer, produire, vendre des produits artisanaux fabriqués à partir de produits naturels (90%)
-(Éléments de la mer (coquillages, sable, coraux…), élément de la terre (graines, calebasses, bois
-flotté…)). De plus, notre entreprise propose un volet « formation » aux métiers de l’artisanat d’art.
-<br />
-                            <br />
-                            Le développement durable, est une alternative inéluctable à notre mode de développement actuel
-qui épuise les ressources naturelles, creuse les écarts de richesse et ampute l’avenir des générations
-actuelles et futures.
-La plupart des produits que nous achetons ont un impact sur l’environnement, car ils nécessitent de
-par leur production, emballage et livraison, des matières premières et de l’énergie.
-                        </p></strong>
+            <div className="BoxContainer">
+                            <strong><p className="styleText" id="TextLorem">
+                                            Afin de porter notre pierre à l’édifice, et pour répondre à un appel à « Entreprendre en lycée »
+                concours organisé par le « GIPDAIFI » nous lycée Hyacinthe Bastaraud de Marie-Galante avons choisi
+                de mettre en place : « La Maison des Artistes », entreprise à caractère commerciale qui a pour but de
+                créer, produire, vendre des produits artisanaux fabriqués à partir de produits naturels (90%)
+                (Éléments de la mer (coquillages, sable, coraux…), élément de la terre (graines, calebasses, bois
+                flotté…). De plus, notre entreprise propose un volet « formation » aux métiers de l’artisanat d’art.
+                <br />
+                                            <br />
+                                            Le développement durable, est une alternative inéluctable à notre mode de développement actuel
+                qui épuise les ressources naturelles, creuse les écarts de richesse et ampute l’avenir des générations
+                actuelles et futures.
+                La plupart des produits que nous achetons ont un impact sur l’environnement, car ils nécessitent de
+                par leur production, emballage et livraison, des matières premières et de l’énergie.
+                                        </p></strong>
                 <div className="boxCont">
-                    <div className="Contact">
-                        <h2 className="legendContactC">Renseignements</h2>
-                        <div className="TextCont">
-                            <p className="styleText"><i className="fa fa-home" aria-hidden="true"></i><b className="StylePol"> Adresse : </b>La Maison des Artistes,<br /> lycée Hyacinthe Bastaraud Rue de la Savane <br />BP 18 97112 Grand-Bourg</p>
-                            <p className="styleText"><i className="fa fa-envelope" aria-hidden="true"></i><b className="StylePol"> Email :</b> lamaisondesartistesmg@gmail.com</p>
-                            <p className="styleText"><i className="fa fa-phone" aria-hidden="true"></i><b className="StylePol" > N° de téléphone :</b> 0690 .981.018</p>
+                        <div className="Contact">
+                            <h2 className="legendContactC">Renseignements</h2>
+                            <div className="TextCont">
+                                <p className="styleText"><i className="fa fa-home -co-m" aria-hidden="true"></i><b className="StylePol">Adresse</b>:<br/>La Maison des Artistes,<br /> lycée Hyacinthe Bastaraud Rue de la Savane <br />BP 18 97112 Grand-Bourg</p>
+                                <p className="styleText"><i className="fa fa-envelope -co" aria-hidden="true"></i><b className="StylePol">Email</b>:<br/>lamaisondesartistesmg@gmail.com</p>
+                                <p className="styleText"><i className="fa fa-phone -co" aria-hidden="true"></i><b className="StylePol"> N° de téléphone</b>:<br/> 0690 .981.018</p>
+                            </div>
                         </div>
-                    </div>
-                
 
-               
-                    <div className="ContainerMap">
-                        <h2 className="legendContactC" id="legendContact">Carte</h2>
-                    <div className="div-iframe-contact">
-                        <iframe className="Mapping" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1026.3353530397678!2d-61.3137995!3d15.884135299999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c6b54b98e31a5c9%3A0xe5f003c862fb434e!2sLyc%C3%A9e+Hyacinthe+Bastaraud!5e0!3m2!1sfr!2sgp!4v1516901128303" ></iframe>
-                    </div>
+                        <div className="boxMap">
+                            <h2 className="legendContactC">Carte</h2>
+                            <Map id="Leaflet-map" center={[this.state.latitude, this.state.longitude]} zoom={this.state.zoom}>
+                                <TileLayer
+                                    attribution='&copy; <a href="https://osm.org/go/YzRpPSJWC--?layers=C&way=521380803">OpenStreetMap</a> contributors'
+                                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                                />
+                                <Marker position={[this.state.latitude, this.state.longitude]}>
+                                </Marker>
+                            </Map>
+                        </div>       
                 </div>
             </div>
-            </div>
+
+
+
         <Footer/>
+
+
         </div>
-
-
         );
     }
 }
